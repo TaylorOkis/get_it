ed295e4760babe81488bd653f4c98afd5b092d19
TaylorOkis <taylorokis@yahoo.com>
2024-09-16 08:01:41 +0100

    Add Section Heading widget

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../common/widgets/custom_shapes/containers/primary_header_container.dart';
 import '../../../../common/widgets/custom_shapes/containers/search_container.dart';
+import '../../../../common/widgets/text/section_heading.dart';
 import '../../../../utils/constants/sizes.dart';
 import 'widgets/home_appbar.dart';
 
@@ -19,7 +20,16 @@ class HomeScreen extends StatelessWidget {
                 children: [
                   GetItHomeAppBar(),
                   SizedBox(height: GetItSizes.spaceBetweenSections),
-                  GetItSearchContainer(text: 'Search in Store')
+                  GetItSearchContainer(text: 'Search in Store'),
+                  SizedBox(height: GetItSizes.spaceBetweenSections),
+                  Padding(
+                    padding: EdgeInsets.only(left: GetItSizes.defaultSpace),
+                    child: Column(
+                      children: [
+                        GetItSectionHeading(title: 'Popular Categories')
+                      ],
+                    ),
+                  )
                 ],
               ),
             ),
