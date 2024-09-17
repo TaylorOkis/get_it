4eca0236aa0bbf982ed83a5155c5cd1f62dcf79c
TaylorOkis <taylorokis@yahoo.com>
2024-09-17 16:01:27 +0100

    Add some image assets.

--- lib/utils/constants/image_strings.dart
+++ lib/utils/constants/image_strings.dart
@@ -1,12 +1,13 @@
 class GetItImages {
-  // App logos
+  // -- App logos
   static const String darkApplogo = "assets/images/logos/";
   static const String lightApplogo = "assets/images/";
 
-  // Social logos
+  // -- Social logos
   static const String google = "assets/logos/google-icon.png";
   static const String facebook = "assets/logos/facebook-icon.png";
 
+  // -- Onboarding images
   static const String onBoardingImage1 =
       "assets/images/on_boarding_images/sammy-line-searching.gif";
   static const String onBoardingImage2 =
@@ -14,8 +15,27 @@ class GetItImages {
   static const String onBoardingImage3 =
       "assets/images/on_boarding_images/sammy-line-delivery.gif";
 
+  // -- Email images
   static const String confirmEmailImage =
       "assets/images/animations/sammy-line-man-receives-a-mail.png";
   static const String successImage =
       "assets/images/animations/sammy-line-success.png";
+
+  // -- Category icons
+  static const String sportIcon =
+      "assets/icons/categories/icons8-bowling-50.png";
+  static const String clothIcon =
+      "assets/icons/categories/icons8-tailors-dummy-50.png";
+  static const String shoeIcon = "assets/icons/categories/icons8-shoes-50.png";
+  static const String cosmeticsIcon =
+      "assets/icons/categories/icons8-cosmetics-50.png";
+  static const String animalIcon =
+      "assets/icons/categories/icons8-dog-heart-64.png";
+  static const String toyIcon = "assets/icons/categories/icons8-toy-car-50.png";
+  static const String furnitureIcon =
+      "assets/icons/categories/icons8-office-chair-50.png";
+  static const String jeweleryIcon =
+      "assets/icons/categories/icons8-sparkling-diamond-50.png";
+  static const String electronicsIcon =
+      "assets/icons/categories/icons8-smartphone-50.png";
 }
